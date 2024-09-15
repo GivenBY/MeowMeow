daa14dab8b51e01cb1e4c885176e0b6c8fbcc68c
Onkar Yaglewad <121476278+once-human@users.noreply.github.com>
2024-09-16 01:37:24 +0530

    setup.py for easier access

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MeowMeow",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "PyGObject",  # For GTK
+    ],
+    entry_points={
+        "console_scripts": [
+            "meowmeow=main:main",  # for command line tool (easy access)
+        ]
+    },
+    author="Your Name",
+    description="A simple wrapper around Groq with GTK 3 GUI",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GivenBY/MeowMeow",
+)
